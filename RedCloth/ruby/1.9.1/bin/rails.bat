b5a7e2ed859134ab00e709b0913527000213c3e2
Yanushik Cerda <yanushikcerda@gmail.com>
2013-04-19 20:29:49 -0500

    Not sure what I was trying

--- RedCloth/ruby/1.9.1/bin/rails.bat
+++ RedCloth/ruby/1.9.1/bin/rails.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "c:/Users/yanus_000/documents/github/simpleblog2/RedCloth/ruby/1.9.1/bin/rails" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"ruby.exe" "C:/Users/yanus_000/Documents/GitHub/simpleblog2/RedCloth/ruby/1.9.1/bin/rails" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
 @"ruby.exe" "%~dpn0" %*
